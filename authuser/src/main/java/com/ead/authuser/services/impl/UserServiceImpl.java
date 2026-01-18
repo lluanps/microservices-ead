d8143d4d729f2c7d3fbf69a4dd203dfa87e53d4f
lluanps <lluanps@gmail.com>
2026-01-18 01:02:59 -0300

    Adicionado métodos para Usuário e feito ajustes no application.yml

---
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repository.UserRepository;
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+}
