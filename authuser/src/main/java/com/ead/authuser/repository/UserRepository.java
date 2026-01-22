c0519e980d397e32552890e1d5f129095526371f
lluanps <lluanps@gmail.com>
2026-01-22 00:31:58 -0300

    Filtros Avançados em APIs com Specification

--- authuser/src/main/java/com/ead/authuser/repository/UserRepository.java
+++ authuser/src/main/java/com/ead/authuser/repository/UserRepository.java
@@ -2,13 +2,14 @@ package com.ead.authuser.repository;
 
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Integer> {
+public interface UserRepository extends JpaRepository<UserModel, Integer>, JpaSpecificationExecutor<UserModel> {
 
     boolean existsByUsername(String username);
 
